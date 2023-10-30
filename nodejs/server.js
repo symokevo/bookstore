1aed2a94cd245be1e663b6a33abe2c2a8aae6de4
Simon Rabuogi <symokevo@gmail.com>
2023-10-30 09:58:17 +0300

    add a listening log text to the createserver function

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 function onRequest(request, response){
+    console.log("server listening...")
     response.writeHead(200, {
         'Content-Type':'text/plain'
     }),
