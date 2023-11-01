4760e1a9c3ab65bb7f2555566573c9d8e23f3207
Simon Rabuogi <symokevo@gmail.com>
2023-11-01 08:39:39 +0300

    configure appjs and make update

--- bookstore-backend/app.js
+++ bookstore-backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const bookRoutes = require('./api/routes/books');
 const userRoutes = require('./api/routes/user');
@@ -7,8 +8,9 @@ const userRoutes = require('./api/routes/user');
 const bodyParser = require('body-parser')
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const dbURI = process.env.dbURI;
 
-mongoose.connect(DB-URI);
+mongoose.connect(dbURI);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
